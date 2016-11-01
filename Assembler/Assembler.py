60569f75c30514bbeab187b237762c6606ae3259
Stepan <biochoker@gmail.com>
2016-11-02 00:59:41 +0300

    Добавил коменты и фикс, ускоряющий инициализацию на 22.8%

--- Assembler/Assembler.py
+++ Assembler/Assembler.py
@@ -13,16 +13,17 @@ parser = argparse.ArgumentParser(description='Assembles reads to genome.')
 parser.add_argument('-r', '--reads', type=str, help='Fasta file with reads for assempling')
 parser.add_argument('-k', type=int, help = 'Length of parts, we set as verticies')
 
+#  Парсинг fastq
 args = parser.parse_args()
-
 reads = loadFastq(args.reads)
 
 tic = time.clock()
 
+#  Инициализация графа
 a = BruijnGraph(reads, args.k)
 
+#  Распечатка
 oldStdout = sys.stdout
-
 sys.stdout = open('Graph.output', 'w')
 print a.edges
 sys.stdout = oldStdout
--- Assembler/BruijnGraph.py
+++ Assembler/BruijnGraph.py
@@ -12,31 +12,34 @@ class BruijnGraph :
 		self._fillEdgedWithSadLonelyEmptiness(reads)
 		self._fillEdgesWithRealShit(reads)
 
-
-
 	def _fillEdgedWithSadLonelyEmptiness(self, reads) :
 		for read in reads:
-			currentPart = read[0:self.k]
-			for ch in read[self.k:] :
-				firstChunk = currentPart[0:self.k-1]
-				secondChunk = currentPart[1:]
-				#  Говнокод
-				if firstChunk not in self.edges :
-					self.edges[firstChunk] = {}
-				if secondChunk not in self.edges :
-					self.edges[secondChunk] = {}
+			#  Проход рамкой размером с вершину графа
+			currentPart = read[0:self.k-1]
+			
+			for ch in read[self.k-1:] :
+				#  Инициализация словаря
+				if currentPart not in self.edges :
+					self.edges[currentPart] = {}
+				#  Присоединяем следующий нуклеотид и удаляем первый для сдвига рамки
 				currentPart = currentPart[1:]
 				currentPart += ch
 
+
 	def _fillEdgesWithRealShit (self, reads) :
 		for read in reads :
+			#  Обозначения ребра графа
 			currentPart = read[0:self.k]
+			
 			for ch in read[self.k:] :
+				#  Обозначение вершин грава
 				firstChunk = currentPart[0:self.k-1]
 				secondChunk = currentPart[1:]
+				#  Инициализация при первом обращении (Медленно, возможно)
 				if secondChunk not in self.edges[firstChunk] :
 					self.edges[firstChunk][secondChunk] = [currentPart, 0]
+				#  Увеличение количества встреч наложения
 				self.edges[firstChunk][secondChunk][1] += 1
-
+				#  Присоединяем следующий нуклеотид и удаляем первый для сдвига рамки
 				currentPart = currentPart[1:]
 				currentPart += ch
