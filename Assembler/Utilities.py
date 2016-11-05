92a2a8b8720f00b8552429864961a8d578fd3d7a
Stepan <biochoker@gmail.com>
2016-11-05 12:02:48 +0300

    Клевые графы

--- Assembler/Utilities.py
+++ Assembler/Utilities.py
@@ -34,9 +34,9 @@ def generate_dot_graph (edges, file_path) :
 		print "digraph G {"
 		for name1, val in edges.items() :
 			for name2, arr in val.items() :
-				#print "{}[label=\"\"]{}[label=\"\"]".format(name1, name2)
-				#print "\t{} -> {};".format(name1, name2, arr[1], arr[0])
-				print "\t{} -> {} [label = \"{}\n{}\"];".format(name1, name2, arr[1], arr[0])
+				print "{}[label=\"\"]{}[label=\"\"]".format(name1, name2)
+				print "\t{} -> {}[label={}];".format(name1, name2, arr[1], arr[0])
+				#print "\t{} -> {} [label = \"{}\n{}\"];".format(name1, name2, arr[1], arr[0])
 		print "}"
 		sys.stdout = old_stdout
 
