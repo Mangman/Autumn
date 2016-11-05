e38538bb169ff709d236f8f944d68875065c5579
Stepan <biochoker@gmail.com>
2016-11-05 03:45:14 +0300

    Добавил отсечение кусочков в графе. Работает не совсем полно и правильно, но дает правильный ответ на данной последовательности и с правильно взятыми порогами

--- Assembler/Assembler.py
+++ Assembler/Assembler.py
@@ -12,8 +12,10 @@ from BruijnGraph import BruijnGraph
 
 
 parser = argparse.ArgumentParser(description='Assembles reads to genome.')
-parser.add_argument('-r', '--reads', type=str, help='Fasta file with reads for assembling')
-parser.add_argument('-k', type=int, help = 'Length of parts, we set as verticies')
+parser.add_argument('-r', '--reads',               type=str, help='Fasta file with reads for assembling')
+parser.add_argument('-k',                          type=int, help = 'Length of parts, we set as verticies')
+parser.add_argument('-dt', '--dead_end_threshold', type=int, default = 0, help = 'Dead end coverage threshold.')
+parser.add_argument('-ct', '--cap_threhold',       type=int, default = 0, help = '\'Cap\' coverage threshold')
 
 #  Парсинг fastq
 args = parser.parse_args()
@@ -29,6 +31,8 @@ with open('Graph.output', 'w') as output :
 	sys.stdout = output
 	sys.stdout = old_stdout
 
+a.condensate()
+a.cut_excessive(args.dead_end_threshold, args.cap_threhold)
 a.condensate()
 generate_dot_graph(a.edges, 'conprsd.dot')
 
--- Assembler/BruijnGraph.py
+++ Assembler/BruijnGraph.py
@@ -5,7 +5,6 @@ import sys
 from collections import defaultdict
 from copy import deepcopy
 
-
 from Utilities import reverse_complimentary 
 
 class BruijnGraph :
@@ -46,7 +45,6 @@ class BruijnGraph :
 		while (True) :
 			names = deepcopy(self.edges.keys())
 			for current in names :
-				print "heh"
 				if len(self.edges[current]) == 1 :#and len(self.incoming[current]) == 1 :
 					succeding = list(self.edges[current])[0]
 					if len(self.incoming[succeding]) == 1 : #and len(self.edges[succeding]) == 1 :
@@ -63,79 +61,40 @@ class BruijnGraph :
 							#print 'compressed'
 
 						self.incoming[succeding].remove(current)
+						del self.incoming[current]
 						del self.edges[current]
-				else :
-					print "outta sequence"
 			if join_counter > old_join_counter :
 				old_join_counter = join_counter
-				print "next"
-			else :
-				print "end"
+
+			#  Только один проход -- можно пофиксить
 			break
 
-	# def condensate (self, condensated) :
-	# 	print "lal"
-	# 	# for next_part in self.edges[start_part].keys():
-	# 	# 	if len(self.edges[next_part].keys()) > 1 :
-	# 	# 		self.condensate(next_part, graph)
-	# 	# 	elif len(self.edges[next_part].keys()) == 1 :
-	# 	# 		next_next_part = self.edges[next_part].keys()[0]
+	#  TODO: выбор из двух тупиков
+	def cut_excessive (self, DEAD_END_THRESHOLD, CAP_THRESHOLD) :
+		names = set(deepcopy(self.edges.keys()))
+		new_names = deepcopy(names)
+		#  Находим тупики не особо оптимально
+		for name in names :
+			temp = self.edges[name]
+			for read in temp :
+				new_names.add(read)
+		names = new_names
 
-	# 	# 		first_connection = self.edges[start_part][next_part]
-	# 	# 		second_connection = self.edges[next_part][next_next_part]
-	# 	# 		first_connection_len = len(first_connection)
-	# 	# 		second_connection_len = len(second_connection)
+		for current in names :
+			# Надстройка - "шапка", но с ней все мутно 
+			if len(self.incoming[current]) == 0 and len(self.edges[current]) == 1 :
+				succeding = self.edges[current].keys()[0]
+				link = self.edges[current][succeding]
 
-	# 	# 		average_coverage = (first_connection[1]*first_connection_len+second_connection[1]*second_connection_len)\
-	# 	# 							/(second_connection_len+first_connection_len)
-	# 	# 		graph[start_part][next_next_part] = [first_connection[0]+second_connection[0][-1], average_coverage]
-	# 	# 		self.condensate(next_part, graph)
-	# 	names = set(self.edges.keys()) 
+				if link[1] < CAP_THRESHOLD :
+					del self.edges[current]
+					self.incoming[succeding].remove(current)
+			#  тупик
+			elif len(self.incoming[current]) == 1 and len(self.edges[current]) == 0 :
+				previous = list(self.incoming[current])[0]
+				link = self.edges[previous][current]
 
-
-	# 	for name in names:
-	# 		if name not in condensated and len(self.edges[name]) == 1 and self.incoming[name][1] == 1 : # or self.incoming[name][1] == 0) :
-	# 			previous = self.incoming[name][0]
-	# 			succeding = self.edges[name].keys()[0]
-	# 			#print previous, name
-	# 			#print self.edges[previous]
-	# 			first_connection = self.edges[previous][name]
-	# 			second_connection = self.edges[name][succeding]
-
-	# 			#Пофиксить
-	# 			condensated[previous][succeding] = [first_connection[0]+second_connection[0][-1], (first_connection[1]+second_connection[1])/2] 
-	# 			#print (succeding, previous, first_connection[0]+second_connection[0][-1])
-	# 			self._lal(previous, succeding, condensated)
-	# 			print 'hah'
-	# 		else :
-	# 			condensated[name] = deepcopy(self.edges[name])
-
-	# def _lal (self, previous, current, condensated) :
-	# 	if current not in condensated and len(self.edges[current]) == 1 and self.incoming[current][1] == 1 :
-	# 		succeding = deepcopy(self.edges[current].keys()[0])
-	# 		#print condensated[previous]
-	# 		first_connection = condensated[previous][current]
-	# 		second_connection = deepcopy(self.edges[current][succeding])
-
-	# 		#Пофиксить
-	# 		del condensated[previous][current]
-	# 		#print condensated[previous]
-	# 		condensated[previous][succeding] = [first_connection[0]+second_connection[0][-1], (first_connection[1]+second_connection[1])/2] 
-	# 		self._lal(previous, succeding, condensated)
-	# 	# elif current not in condensated and len(self.edges[current]) > 1 and self.incoming[current][1] == 1 :
-	# 	# 	first_connection = condensated[previous][current]
-	# 	# 	second_connection = self.edges[current][succeding]
-
-	# 	# 	#Пофиксить
-	# 	# 	del condensated[previous][current]
-	# 	# 	condensated[previous][succeding] = [first_connection[0]+second_connection[0][-1], (first_connection[1]+second_connection[1])/2] 
-	# 	# 	self._lal(previous, succeding, condensated)
-	# 	else :
-	# 		condensated[current] = deepcopy(self.edges[current])
-	# 		for name in self.edges[current] :
-	# 			#print self.edges[current]
-	# 			#print name
-	# 			#print '\n'
-	# 			self._lal(current, name, condensated)
-
-
+				if link[1] < DEAD_END_THRESHOLD :
+					del self.edges[current]
+					del self.incoming[current]
+					del self.edges[previous][current]
